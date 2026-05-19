b2372d52d6aa54e91ccbc2d29642079ca374cc22
ngyenngoc <ng.yenngoc.work@gmail.com>
2026-05-19 17:51:10 +0700

    Update MainWindowController and button active

--- src/client/controller/MainWindowController.java
+++ src/client/controller/MainWindowController.java
@@ -3,6 +3,7 @@ package client.controller;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.event.ActionEvent;
 import java.io.IOException;
@@ -12,14 +13,41 @@ public class MainWindowController {
 
     @FXML
     private StackPane centerContent;
+    @FXML private Button btnDashboard;
+    @FXML private Button btnMaterialGroup;
+    @FXML private Button btnCustomer;
+    @FXML private Button btnOrder;
+    @FXML private Button btnSupplier;
+    @FXML private Button btnChat; // Nút Chat nội bộ
+    @FXML private Button btnHome;
+
+    @FXML private javafx.scene.layout.VBox subMenuMaterial;
+    @FXML private Button btnMaterial; // Menu con 1
+    @FXML private Button btnCategory; // Menu con 2
 
     @FXML
     public void initialize() {
         // Mặc định hiện trang chủ hoặc trang khi vừa mở máy
+        handleShowHome(null);
     }
+    private void setActiveButton(Button clickedButton) {
+        Button[] allButtons = {btnDashboard, btnMaterialGroup, btnCustomer, btnOrder, btnSupplier,btnHome, btnDashboard, btnChat, btnMaterial, btnCategory};
+
+        for (Button btn : allButtons) {
+            if (btn != null) {
+                btn.getStyleClass().remove("menu-button-active");
+            }
+        }
+
+        if (clickedButton != null) {
+            clickedButton.getStyleClass().add("menu-button-active");
+        }
+    }
+
 
     @FXML
     private void handleShowCustomer(ActionEvent event) {
+        setActiveButton(btnCustomer);
         try {
             // Cách viết này an toàn hơn để tìm file trong Resource Folders
             URL fxmlLocation = getClass().getResource("/client/view/CustomerView.fxml");
@@ -42,6 +70,7 @@ public class MainWindowController {
 
     @FXML
     private void handleShowMaterial(ActionEvent event) {
+        setActiveButton(btnMaterial);
         try {
             // Cách viết này an toàn hơn để tìm file trong Resource Folders
             URL fxmlLocation = getClass().getResource("/client/view/MaterialView.fxml");
@@ -64,6 +93,7 @@ public class MainWindowController {
 
      @FXML
     private void handleShowCategory(ActionEvent event) {
+         setActiveButton(btnCategory);
         try {
             // Cách viết này an toàn hơn để tìm file trong Resource Folders
             URL fxmlLocation = getClass().getResource("/client/view/CategoryView.fxml");
@@ -86,6 +116,7 @@ public class MainWindowController {
     }
     @FXML
     private void handleShowSupplier(ActionEvent event) {
+        setActiveButton(btnSupplier);
         try {
             // Cách viết này an toàn hơn để tìm file trong Resource Folders
             URL fxmlLocation = getClass().getResource("/client/view/SupplierView.fxml");
@@ -129,6 +160,7 @@ public class MainWindowController {
 
     @FXML
     private void handleShowOrders(ActionEvent event) {
+        setActiveButton(btnOrder);
         try {
             // Cách viết này an toàn hơn để tìm file trong Resource Folders
             URL fxmlLocation = getClass().getResource("/client/view/OrdersView.fxml");
@@ -151,6 +183,7 @@ public class MainWindowController {
     }
     @FXML
     private void handleShowSalesOrder(ActionEvent event) {
+        setActiveButton(btnOrder);
         try {
             // Cách viết này an toàn hơn để tìm file trong Resource Folders
             URL fxmlLocation = getClass().getResource("/client/view/SalesOrderView.fxml");
@@ -172,11 +205,10 @@ public class MainWindowController {
         }
     }
 
-    @FXML
-    private javafx.scene.layout.VBox subMenuMaterial;
 
     @FXML
     public void toggleMaterialMenu(javafx.event.ActionEvent event) {
+        setActiveButton(btnMaterialGroup);
         if (subMenuMaterial != null) {
             boolean isVisible = subMenuMaterial.isVisible();
             subMenuMaterial.setVisible(!isVisible);
@@ -186,13 +218,14 @@ public class MainWindowController {
 
     @FXML
     private void handleShowDashboard(ActionEvent event) {
+        setActiveButton(btnDashboard);
         try {
             // Cách viết này an toàn hơn để tìm file trong Resource Folders
             URL fxmlLocation = getClass().getResource("/client/view/DashboardView.fxml");
 
 
             if (fxmlLocation == null) {
-                throw new IOException("Không tìm thấy file DashboardlView.fxml tại thư mục client.view!");
+                throw new IOException("Không tìm thấy file DashboardView.fxml tại thư mục client.view!");
             }
 
             FXMLLoader loader = new FXMLLoader(fxmlLocation);
@@ -208,6 +241,7 @@ public class MainWindowController {
     }
     @FXML
     private void handleOpenChat() {
+        setActiveButton(btnChat);
         try {
             // Tải file giao diện Chat từ thư mục view
             javafx.fxml.FXMLLoader loader = new javafx.fxml.FXMLLoader(getClass().getResource("/client/view/ChatView.fxml"));
@@ -234,6 +268,22 @@ public class MainWindowController {
             alert.setHeaderText(null);
             alert.setContentText("Không thể mở cửa sổ Chat nội bộ! Vui lòng kiểm tra lại đường dẫn file FXML.");
             alert.showAndWait();
+        }
+    }
+
+    @FXML
+    private void handleShowHome(ActionEvent event) {
+        setActiveButton(btnHome);
+        try {
+            URL fxmlLocation = getClass().getResource("/client/view/HomeView.fxml");
+
+            FXMLLoader loader = new FXMLLoader(fxmlLocation);
+            Parent homeView = loader.load();
+
+            centerContent.getChildren().setAll(homeView);
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
